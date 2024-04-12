c16e373b73a2bf4f1b9a93b4358faa9d54069cac
JParamade <114541314+JParamade@users.noreply.github.com>
2024-04-12 04:00:27 +0200

    Book State Machine

--- SantJordiJam2024/Assets/_Scripts/UI/MenuCanvasController.cs
+++ SantJordiJam2024/Assets/_Scripts/UI/MenuCanvasController.cs
@@ -4,5 +4,17 @@ using UnityEngine;
 
 public class MenuCanvasController : MonoBehaviour
 {
-    
+    private Animator cmpAnimator;
+
+    private void Start() {
+        cmpAnimator = GetComponent<Animator>();
+    }
+
+    public void StartGame() {
+
+    }
+
+    public void NextPage(int pageNumber) {
+
+    }
 }
