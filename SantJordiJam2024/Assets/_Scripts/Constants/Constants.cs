cb5d025af240c81b7ab2a7bc1720056683856512
JParamade <114541314+JParamade@users.noreply.github.com>
2024-04-15 01:27:35 +0200

    Menu Workflow Programmed

---
+++ SantJordiJam2024/Assets/_Scripts/Constants/Constants.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UINames {
+    public static readonly string UI_PLAY = "";
+    public static readonly string UI_SETTINGS = "";
+    public static readonly string UI_CREDITS = "";
+}
--- SantJordiJam2024/Assets/_Scripts/UI/MenuCanvasController.cs
+++ SantJordiJam2024/Assets/_Scripts/UI/MenuCanvasController.cs
@@ -1,20 +1,62 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Rendering;
 using UnityEngine;
 
 public class MenuCanvasController : MonoBehaviour
 {
     private Animator cmpAnimator;
+    private Animator cmpCameraAnimator;
+
+    public GameObject[] panels;
+    private int panelSelector = 0;
+
+    private bool startTimer = false;
+    private float timerTime = 0.1f;
 
     private void Start() {
         cmpAnimator = GetComponent<Animator>();
+        cmpCameraAnimator = Camera.main.GetComponent<Animator>();
     }
 
-    public void StartGame() {
+    private void Update() {
+        if (startTimer) {
+            timerTime -= Time.deltaTime;
 
+            if (timerTime <= 0) {
+                SetActiveUI();
+            }
+        }
     }
 
-    public void NextPage(int pageNumber) {
+    public void GameState(bool gameState) {
+        cmpAnimator.SetInteger("page", 0);
+        cmpAnimator.SetBool("startGame", gameState);
+    }
 
+    public void SwapPage(int pageNumber) {
+        cmpAnimator.SetInteger("page", pageNumber);
+    }
+
+    public void PanelSelector(int panel) { panelSelector = panel; }
+    
+    public void SetTimer() {
+        startTimer = true;
+    }
+
+    public void SetActiveUI() {
+        startTimer = false;
+        timerTime = 0.1f;
+
+        if (cmpAnimator.GetCurrentAnimatorStateInfo(0).speed < 0) {
+            foreach (GameObject panel in panels) {
+                panel.SetActive(false);
+            }
+        }
+        else { panels[panelSelector].SetActive(true); }
+    }
+
+    public void BookOpened() {
+        cmpCameraAnimator.SetBool("zoom", cmpAnimator.GetCurrentAnimatorStateInfo(0).speed > 0 ? true : false);
     }
 }
